1f2545261ea761910b802e49b042c06764fbdbf5
Abhishek0-7 <118382627+Abhishek0-7@users.noreply.github.com>
2025-06-29 22:43:46 +0530

    Create vectorization.py

---
+++ src/vectorization.py
@@ -0,0 +1,23 @@
+# vectorization.py
+
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+
+def create_tfidf_vectorizer(**kwargs):
+    return TfidfVectorizer(
+        max_features=5000,
+        min_df=2,
+        max_df=0.8,
+        ngram_range=(1,2),
+        stop_words='english',
+        **kwargs
+    )
+
+def create_count_vectorizer(**kwargs):
+    return CountVectorizer(
+        max_features=5000,
+        min_df=2,
+        max_df=0.8,
+        ngram_range=(1,1),
+        stop_words='english',
+        **kwargs
+    )
